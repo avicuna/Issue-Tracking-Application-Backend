0ddc4806914d22aacbb2fcf2dd1b3bb2e79ff174
Alejandro Vicuna <vicunaalejandro@gmail.com>
2021-07-13 18:06:30 -0400

    Add initial Junit tests and add the maven compiler plugin to recognize java 11.
    The maven compiler plugin was an automatic solution for Intellij to fix a problem. Not too sure what it means but I'm able to run the app locally.

---
+++ src/test/java/com/alexvicuna/controller/IssueControllerTest.java
@@ -0,0 +1,22 @@
+package com.alexvicuna.controller;
+
+import org.junit.jupiter.api.Test;
+
+class IssueControllerTest {
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void getAllIssues() {
+    }
+
+    @Test
+    void createIssue() {
+    }
+
+    @Test
+    void deleteIssue() {
+    }
+}
---
+++ src/test/java/com/alexvicuna/service/IssueServiceTest.java
@@ -0,0 +1,22 @@
+package com.alexvicuna.service;
+
+import org.junit.jupiter.api.Test;
+
+class IssueServiceTest {
+
+    @Test
+    void getIssue() {
+    }
+
+    @Test
+    void getAllIssues() {
+    }
+
+    @Test
+    void createIssue() {
+    }
+
+    @Test
+    void deleteIssue() {
+    }
+}
